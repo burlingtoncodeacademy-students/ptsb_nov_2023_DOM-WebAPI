d0fa350867e5f3747fd536242f193ed87191a3ad
u4ik <insighteuphoric@gmail.com>
2024-01-02 19:41:13 -0500

    bubbling example

---
+++ IntroToDom/bubbling/index.js
@@ -0,0 +1,13 @@
+let button = document.getElementById("button");
+let container = document.getElementById("container");
+
+function first(e) {
+  e.stopPropagation();
+  alert(1);
+}
+function second() {
+  alert(2);
+}
+
+button.addEventListener("click", first);
+container.addEventListener("click", second);
