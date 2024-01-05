7610e7583b42b97157998fe48711081c5df0a7f3
u4ik <insighteuphoric@gmail.com>
2024-01-04 20:17:15 -0500

    finished todo

--- IntroToDom/todolist/dom_todo_template/index.js
+++ IntroToDom/todolist/dom_todo_template/index.js
@@ -19,13 +19,36 @@ todoEmojis.forEach((i) => {
   emojiSelector.appendChild(option);
 });
 
+todoForm.addEventListener("submit", (e) => {
+  //   Prevent default refresh of the page, ex. clicking submit on a form
+  e.preventDefault();
+
+  if (todosInputField.value.length === 0) {
+    alert("Please provide a todo!");
+  } else if (todosInputField.value.length > 15) {
+    alert("Todo too long!");
+    todosInputField.value = "";
+  } else {
+    const formData = {
+      name: todoForm.todo.value,
+      icon: todoForm.emoji.value,
+    };
+
+    newArr.reverse().push(formData);
+    createTodos(newArr.reverse());
+    todoForm.reset();
+    todoForm[0].focus();
+  }
+});
+
 function createTodos(filterArr) {
+  // Refreshes our list data
   while (todosList.firstChild) {
     todosList.removeChild(todosList.firstChild);
   }
 
   filterArr.forEach((item, idx) => {
-    console.log(item);
+    // console.log(item);
     // Randomizing values for background color of the todo
     const random = (min, max) => {
       return Math.floor(Math.random() * (max - min) + min);
@@ -49,8 +72,16 @@ function createTodos(filterArr) {
     listItem.classList.add("todo");
     emoji.classList.add("emoji");
     toDoText.classList.add("todo-text");
+    deleteButton.classList.add("todo-delete");
 
     listItem.style.backgroundColor = `rgba(${randomColor1},${randomColor2},${randomColor3})`;
+
+    // Handling the delete functionality
+    deleteButton.addEventListener("click", () => {
+      console.log("Deleted the todo");
+      newArr = filterArr.filter((i) => i !== item);
+      createTodos(newArr);
+    });
 
     listItem.appendChild(emoji);
     listItem.appendChild(toDoText);
@@ -58,6 +89,12 @@ function createTodos(filterArr) {
 
     todosList.appendChild(listItem);
   });
+
+  if (newArr.length === 0) {
+    let noToDosText = document.createElement("h1");
+    noToDosText.textContent = "No to dos :)";
+    todosList.appendChild(noToDosText);
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
