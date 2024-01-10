0d3f31b44852a33d58ea13858b7b68bff216a00b
u4ik <insighteuphoric@gmail.com>
2024-01-10 18:21:00 -0500

    pokedex challenge instructions added

--- pokedex-challenge/index.js
+++ pokedex-challenge/index.js
@@ -1,1 +1,27 @@
-console.log("test");
+/*
+?   Challenge:
+        - Grab a hold of the HTML elements that are necessary
+        - Use the https://pokeapi.co/ to retrieve the information of a pokemon
+        - Then using the input field, a user should be able to type in the pokemon name or number.
+
+        *HINT There is a method you can use to grab a hold of the value contained within the input field
+
+        - The button with the text of "Look", should execute a fetch to the Pokemon API
+        The API should return the exact data for the pokemon-name/# that was provided to the input field
+
+        * You will only need to fetch to one endpoint
+
+        Display the results within each respected html element
+
+        * Be sure to understand the data type you are working with to display the results correctly
+
+        - Name
+        - Image
+        - Stats
+        - Moves
+
+        Bonus* 
+        - When a user goes and types in another pokemon-name/#, the moves and stats keep stacking on top of the previous data.
+        
+        Handle clearing out the past data to present the new data.
+*/
